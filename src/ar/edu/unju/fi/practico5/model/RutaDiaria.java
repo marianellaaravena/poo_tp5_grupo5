fe170f20f1844690f6a16c6301f0e30a1545c5d0
Julieta Garcia <juligarcia.fl@gmail.com>
2026-09-17 18:29:31 -0300

    Implemento de las operaciones de la clase RutaDiaria

--- src/ar/edu/unju/fi/practico5/model/RutaDiaria.java
+++ src/ar/edu/unju/fi/practico5/model/RutaDiaria.java
@@ -1,5 +1,6 @@
 package ar.edu.unju.fi.practico5.model;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -7,4 +8,30 @@ public class RutaDiaria {
     private Date fecha;
     private Vehiculo vehiculo;
     private List<Envio> envios;
+    
+    public RutaDiaria(Date fecha, Vehiculo vehiculo) { 
+		this.fecha = fecha; 
+		this.vehiculo = vehiculo; 
+		this.envios = new ArrayList<>(); 
+	}
+    
+    // Metodo para agregar un envio
+    public void agregarEnvio(Envio envio) { 
+		this.envios.add(envio); 
+	}
+    
+    // ========= Metodos Auxiliares =========
+    // Calcula el peso acumulado
+    public double getPesoActualTotal() { 
+		double pesoAcumulado = 0.0; 
+		for (Envio e : envios) { 
+			pesoAcumulado += e.getPesoTotal(); 
+		} 
+		return pesoAcumulado; 
+	}
+    
+    // Getters
+    public Date getFecha() { return fecha; } 
+	public Vehiculo getVehiculo() { return vehiculo; } 
+	public List<Envio> getEnvios() { return envios; }
 }
