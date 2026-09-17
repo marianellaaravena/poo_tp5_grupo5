42b796c9565dbbcd099985198ac4cd5a7801f5ad
Julieta Garcia <juligarcia.fl@gmail.com>
2026-09-17 18:09:32 -0300

    Implemento de las operaciones de la clase Vehiculo

--- src/ar/edu/unju/fi/practico5/model/Vehiculo.java
+++ src/ar/edu/unju/fi/practico5/model/Vehiculo.java
@@ -4,4 +4,15 @@ public class Vehiculo {
     private String patente;
     private double capacidadPeso;
     private double capacidadVolumen; 
+    
+    public Vehiculo(String patente, double capacidadPeso, double capacidadVolumen) { 
+		this.patente = patente; 
+		this.capacidadPeso = capacidadPeso; 
+		this.capacidadVolumen = capacidadVolumen; 
+	}
+    
+    // Getters
+    public String getPatente() { return patente; } 
+	public double getCapacidadPeso() { return capacidadPeso; } 
+	public double getCapacidadVolumen() { return capacidadVolumen; }
 }
