45eccce6c84b8f86aaa9d732f05c6a4a62886cbf
Julieta Garcia <juligarcia.fl@gmail.com>
2026-09-17 18:04:31 -0300

    Implemento de las operaciones de la clase Paquete

--- src/ar/edu/unju/fi/practico5/model/Paquete.java
+++ src/ar/edu/unju/fi/practico5/model/Paquete.java
@@ -5,4 +5,22 @@ public class Paquete {
     private String descripcion;
     private double peso;
     private double volumen;
+    
+    public Paquete(String codigoPaquete, String descripcion, double peso, double volumen) {
+		this.codigoPaquete = codigoPaquete;
+		this.descripcion = descripcion;
+		this.peso = peso;
+		this.volumen = volumen;
+	}
+    
+    // Getters
+    public String getCodigo() { return codigoPaquete; } 
+	public String getDescripcion() { return descripcion; } 
+	public double getPeso() { return peso; } 
+	public double getVolumen() { return volumen; } 
+	
+	@Override 
+	public String toString() { 
+		return "Paquete[" + codigoPaquete + " | " + descripcion + " | " + peso + "kg | " + volumen + "dm3]"; 
+	}
 }
